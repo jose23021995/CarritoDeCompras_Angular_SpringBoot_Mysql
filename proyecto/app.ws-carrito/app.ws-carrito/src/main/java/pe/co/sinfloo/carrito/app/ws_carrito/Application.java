4df76ebf79d4bb55b464f431eda6bfb9cf01a625
jose23021995 <80795569+jose23021995@users.noreply.github.com>
2026-03-31 17:20:22 -0600

    se hacen configuraciones iniciales de backend en spring boot y eclipce

---
+++ proyecto/app.ws-carrito/app.ws-carrito/src/main/java/pe/co/sinfloo/carrito/app/ws_carrito/Application.java
@@ -0,0 +1,13 @@
+package pe.co.sinfloo.carrito.app.ws_carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
